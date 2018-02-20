dd416d75d9cb0efd45ff21667e30d5b5c7130dac
arlexio <agole@andrew.cmu.edu>
2018-02-20 11:34:44 -0500

    part 3

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -10,7 +10,6 @@
 //         console.log('test');
 // });
 // });
-
 $(document).on("click", ".delete-item", function() {
     $(this).parent().remove();
 });
@@ -19,4 +18,12 @@ $(document).on("click", "#add-item", function() {
     var list = $("#grocery-list");
     var itemInput = $("#new-list-item");
     list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+});
+
+$(document).on("click", ".complete-item", function() {
+    if ($(this).is(':checked')) { 
+        $(this).parent().css('text-decoration', 'line-through');
+    } else {
+        $(this).parent().css('text-decoration', 'none');
+    }
 });
